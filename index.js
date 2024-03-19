ec2ef0f84369f6260b2ebb1c0db4585c8f9203f1
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-19 04:35:58 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+function result(num1, num2) {
+    return num1 * num2;
+}
+const num1 = 31;
+const num2 = 2;
+const multiply = result(num1, num2);
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+const numbers = [10, 20, 15];
+const max = Math.max(...numbers);
+console.log(max);
